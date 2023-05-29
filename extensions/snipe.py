9d81adbfea40a3739e119cf44a271d6abb52c0d0
Evanlau1798 <sl30608@gmail.com>
2023-05-29 21:39:45 +0800

    完善snipe功能

--- extensions/snipe.py
+++ extensions/snipe.py
@@ -5,7 +5,8 @@ from discord import SlashCommandOptionType as type
 from utils.EmbedMessage import SakuraEmbedMsg
 from collections import deque
 
-snipes = {} # 建立一個字典，用來儲存每個伺服器最近刪除或編輯的訊息
+delete_snipes = {} # 建立兩個字典，用來儲存每個伺服器最近刪除和編輯的訊息
+edited_snipes = {}
 MAX_MESSAGES = 100 # 定義一個常數，表示每個伺服器能儲存的訊息數量上限
 
 class SnipeEventsListener(commands.Cog):
@@ -16,13 +17,19 @@ class SnipeEventsListener(commands.Cog):
     @option("index", type=type.integer, description="頁數索引", required=False)
     @default_permissions(administrator=True)
     async def deletesnipe(self,message: discord.ApplicationContext, index: int = 0):
-        if message.guild.id in snipes:
-            queue = snipes[message.guild.id]
+        if message.guild.id in delete_snipes:
+            queue = delete_snipes[message.guild.id]
             if 0 <= index < len(queue): # 檢查索引是否有效（介於 0 到佇列長度 - 1 之間）
                 snipe_message:discord.Message = queue[index]
-                embed = SakuraEmbedMsg(description=snipe_message.content)
-                embed.set_author(name=snipe_message.author.display_name, icon_url=snipe_message.author.avatar.url)
-                await message.respond(embed=embed)
+                embed = SakuraEmbedMsg()
+                embed.set_author(name="訊息刪除紀錄", icon_url=snipe_message.author.avatar.url)
+                embed.add_field(name="使用者",value=snipe_message.author.mention,inline=False)
+                embed.add_field(name="發送至的頻道",value=snipe_message.channel.mention,inline=False)
+                embed.add_field(name="訊息內容",value=snipe_message.content,inline=False)
+                embed.add_field(name="發送時間",value=f"{snipe_message.created_at}\n<t:{int(snipe_message.created_at.timestamp())}>",inline=False)
+                if len(snipe_message.attachments) != 0:
+                    embed.set_image(url=snipe_message.attachments[0])
+                await message.respond(embed=embed, ephemeral=True)
             else:
                 await message.respond(f"索引超出範圍（0-{len(queue)-1}）", ephemeral=True)
         else:
@@ -32,13 +39,21 @@ class SnipeEventsListener(commands.Cog):
     @option("index", type=type.integer, description="頁數索引", required=False)
     @default_permissions(administrator=True)
     async def editsnipe(self,message: discord.ApplicationContext, index: int = 0):
-        if message.guild.id in snipes:
-            queue = snipes[message.guild.id]
+        if message.guild.id in edited_snipes:
+            queue = edited_snipes[message.guild.id]
             if 0 <= index < len(queue): # 檢查索引是否有效（介於 0 到佇列長度 - 1 之間）
-                snipe_message:discord.Message = queue[index]
-                embed = SakuraEmbedMsg(description=snipe_message.content)
-                embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-                await message.respond(embed=embed)
+                before_snipe_message,after_snipe_message = queue[index]
+                embed = SakuraEmbedMsg()
+                embed.set_author(name="訊息編輯紀錄", icon_url=before_snipe_message.author.avatar.url)
+                embed.add_field(name="使用者",value=before_snipe_message.author.mention,inline=False)
+                embed.add_field(name="發送至的頻道",value=before_snipe_message.channel.mention,inline=False)
+                embed.add_field(name="編輯前內容",value=before_snipe_message.content,inline=False)
+                embed.add_field(name="編輯後內容",value=after_snipe_message.content,inline=False)
+                embed.add_field(name="發送時間",value=f"{before_snipe_message.created_at}\n<t:{int(before_snipe_message.created_at.timestamp())}>",inline=False)
+                embed.add_field(name="編輯時間",value=f"{after_snipe_message.edited_at}\n<t:{int(after_snipe_message.edited_at.timestamp())}>",inline=False)
+                if len(before_snipe_message.attachments) != 0:
+                    embed.set_image(url=before_snipe_message.attachments[0])
+                await message.respond(embed=embed, ephemeral=True)
             else:
                 await message.respond(f"索引超出範圍（0-{len(queue)-1}）", ephemeral=True)
         else:
@@ -49,28 +64,28 @@ class SnipeEventsListener(commands.Cog):
         print(message.content)
         if message.author.bot:return
         guild_id = message.guild.id
-        if guild_id in snipes:
-            queue = snipes[guild_id]
+        if guild_id in delete_snipes:
+            queue = delete_snipes[guild_id]
             queue.appendleft(message)
             if len(queue) > MAX_MESSAGES:
                 queue.pop()
         else:
             queue = deque([message]) # 如果該伺服器沒有儲存的訊息，則建立一個新的雙端佇列，並將被刪除的訊息加入其中
-            snipes[guild_id] = queue
+            delete_snipes[guild_id] = queue
 
     @commands.Cog.listener()
     async def on_message_edit(self,before:discord.Message, after:discord.Message):
         print("舊:",before.content,"新:",after.content)
         if before.author.bot:return
         guild_id = before.guild.id
-        if guild_id in snipes:
-            queue = snipes[guild_id]
-            queue.appendleft(before)
+        if guild_id in edited_snipes:
+            queue = edited_snipes[guild_id]
+            queue.appendleft((before,after))
             if len(queue) > MAX_MESSAGES: # 如果佇列的長度超過上限，則移除佇列的右側元素
                 queue.pop()
         else:
-            queue = deque([before]) # 如果該伺服器沒有儲存的訊息，則建立一個新的雙端佇列，並將被編輯的訊息加入其中
-            snipes[guild_id] = queue
+            queue = deque([(before,after)]) # 如果該伺服器沒有儲存的訊息，則建立一個新的雙端佇列，並將被編輯的訊息加入其中
+            edited_snipes[guild_id] = queue
 
 
 def setup(bot:discord.Bot):
